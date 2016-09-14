996768f8badc2144eeb96489b67cc3d3c4d92de0
Dmitriy Komarov <ehpgms@gmail.com>
2016-09-14 09:28:38 +0400

    Small fixes

--- backend/src/main/java/ssau/ru/BackendApplication.java
+++ backend/src/main/java/ssau/ru/BackendApplication.java
@@ -1,18 +1,11 @@
 package ssau.ru;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication implements CommandLineRunner {
+public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(System.getProperty("java.class.path"));
-    }
 }
