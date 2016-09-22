b4070a94f9b1868a00c4801c1a9ad939efb94b94
dmitry <ehpgms@gmail.com>
2016-09-22 21:56:20 +0400

    Fix edges serialization

--- backend/src/main/java/ssau/ru/RepositoryConfig.java
+++ backend/src/main/java/ssau/ru/RepositoryConfig.java
@@ -1,15 +1,8 @@
 package ssau.ru;
 
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.module.SimpleSerializers;
-import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
-import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
-import com.fasterxml.jackson.databind.util.NameTransformer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import ssau.ru.graph.PathEdge;
 import ssau.ru.graph.PathNode;
 
 @Configuration
@@ -17,40 +10,5 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(PathNode.class);
-    }
-
-    @Override
-    public void configureJacksonObjectMapper(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new Module() {
-            @Override
-            public String getModuleName() {
-                return "custom.module";
-            }
-
-            @Override
-            public Version version() {
-                return Version.unknownVersion();
-            }
-
-            @Override
-            public void setupModule(SetupContext context) {
-//                SimpleSerializers serializers = new SimpleSerializers();
-//                serializers.addSerializer(PathNode.class, new PathEdge.NodeSerializer());
-//                context.addSerializers(serializers);
-
-                context.addBeanSerializerModifier(new BeanSerializerModifier() {
-                    @Override
-                    public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc,
-                                                              JsonSerializer<?> serializer) {
-                        beanDesc.
-                        if (beanDesc.getBeanClass().equals(PathNode.class)) {
-                            return new PathEdge.BeanIdOnlySerializer((BeanSerializerBase) serializer,
-                                    NameTransformer.NOP);
-                        }
-                        return serializer;
-                    }
-                });
-            }
-        });
     }
 }
--- backend/src/main/java/ssau/ru/graph/PathEdge.java
+++ backend/src/main/java/ssau/ru/graph/PathEdge.java
@@ -1,23 +1,8 @@
 package ssau.ru.graph;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.annotation.JsonValueInstantiator;
-import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
-import com.fasterxml.jackson.databind.ser.impl.UnwrappingBeanSerializer;
-import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.fasterxml.jackson.databind.util.NameTransformer;
-import org.springframework.data.rest.core.config.Projection;
+import com.fasterxml.jackson.annotation.*;
 
 import javax.persistence.*;
-import java.io.IOException;
 
 @Entity
 @Table(name = "EDGES")
@@ -27,18 +12,14 @@ public class PathEdge {
 
     @ManyToOne
     @JoinColumn(name = "edge_from")
-//    @JsonSerialize(using = NodeSerializer.class)
-//    @JsonIgnore
     private PathNode from;
 
     @ManyToOne
     @JoinColumn(name = "edge_to")
-//    @JsonIgnore
     private PathNode to;
 
     @ManyToOne
     @JoinColumn(name = "graph_owner")
-    @JsonIgnore
     private PathGraph graph;
 
     public PathEdge() {}
@@ -53,6 +34,7 @@ public class PathEdge {
         return id;
     }
 
+    @JsonIgnore
     public PathNode getFrom() {
         return from;
     }
@@ -61,6 +43,7 @@ public class PathEdge {
         this.from = from;
     }
 
+    @JsonIgnore
     public PathNode getTo() {
         return to;
     }
@@ -77,40 +60,13 @@ public class PathEdge {
         this.graph = graph;
     }
 
-    public static class NodeSerializer extends StdSerializer<PathNode> {
-        public NodeSerializer() {
-            this(null);
-        }
-
-        protected NodeSerializer(Class<PathNode> t) {
-            super(t);
-        }
-
-        @Override
-        public void serialize(PathNode value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeNumber(value.getId());
-        }
+    @JsonProperty("from")
+    public Long getFromId() {
+        return from == null ? null : from.getId();
     }
 
-    public static class BeanIdOnlySerializer extends UnwrappingBeanSerializer {
-        public BeanIdOnlySerializer(BeanSerializerBase src, NameTransformer transformer) {
-            super(src, transformer);
-        }
-
-        @Override
-        public JsonSerializer<Object> unwrappingSerializer(NameTransformer nameTransformer) {
-            return new BeanIdOnlySerializer(this, nameTransformer);
-        }
-
-        @Override
-        public void serializeFields(Object bean, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            PathNode node = (PathNode) bean;
-            gen.writeNumberField("from", node.getId());
-        }
-
-        @Override
-        public boolean isUnwrappingSerializer() {
-            return true;
-        }
+    @JsonProperty("to")
+    public Long getToId() {
+        return to == null ? null : to.getId();
     }
 }
